77697a963ed598a89fcb053a081f4cb04d425fd8
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-01 10:59:46 -1200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,33 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (!Array.isArray(newGrades)) {
     return [];
   }
 
-  const studentsInCity = students.filter(student => student.location === city);
+  const stCity = students.filter((student) => student.location === city);
 
-  const updatedStudents = studentsInCity.map(student => {
-    const matchedGrade = newGrades.find(grade => grade.studentId === student.id);
+  const studentsGraded = stCity.map((student) => {
+    const gradeFilter = newGrades.filter(
+      (newGrade) => newGrade.studentId === student.id,
+    );
 
-    const grade = matchedGrade ? matchedGrade.grade : 'N/A';
+    let grade;
+
+    if (gradeFilter[0]) {
+      grade = gradeFilter[0].grade;
+    } else {
+      grade = 'N/A';
+    }
 
     return {
       ...student,
-      grade
+      grade,
     };
   });
 
-  return updatedStudents;
+  return studentsGraded;
 }
 
 export default updateStudentGradeByCity;
