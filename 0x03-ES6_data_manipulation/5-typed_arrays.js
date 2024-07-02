1d780e9ef4865afd529d67dcac66cfe29b86ad05
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-01 23:50:42 -1200

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const viewer = new DataView(buffer);
   try {
-    const bf = new ArrayBuffer(length);
-    const int8 = new DataView(bf);
-    int8.setInt8(position, value);
-    return int8;
-  } catch (err) {
-    throw Error('Position outside range');
+    viewer.setUint8(position, value);
+  } catch (RangeError) {
+    throw new Error('Position outside range');
   }
+  return (viewer);
 }
