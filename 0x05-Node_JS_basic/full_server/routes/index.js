2d837f2d9824722f8256e8836b1c6b82dfe2bc16
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-20 10:06:01 -1200

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,8 +1,22 @@
+import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-export default function (app) {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  router.get('/', (req, res) => {
+    AppController.getHomepage(req, res);
+  });
+
+  router.get('/students', (req, res) => {
+    StudentsController.getAllStudents(req, res, process.argv[2]);
+  });
+
+  router.get('/students/:major', (req, res) => {
+    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+  });
 }
+
+export default controllerRouting;
