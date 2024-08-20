7b52654d2d4e2d8b5768dbcaebb7d6e234fa208f
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-20 07:18:16 -1200

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -4,21 +4,29 @@ function readDatabase(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(err);
+        reject(Error(err));
         return;
       }
+      const content = data.toString().split('\n');
 
-      const content = data.split('\n').filter(Boolean).map(item => item.split(','));
-      const [header, ...students] = content;  // Destructure to separate header from students
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
 
       const fields = {};
-      students.forEach((student) => {
-        const field = student[3];
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(student[0]);
-      });
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
 
       resolve(fields);
+
+      //   return fields;
     });
   });
 }
