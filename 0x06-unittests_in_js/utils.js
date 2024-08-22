157115dd7857da042046f49cc8e4b7c2b56378a4
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-22 11:05:33 -1200

    Update utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,28 +1,35 @@
 const Utils = {
-  isNegZero(n) {
-    const num = Number(n);
-    return num === 0 && 1 / num === -Infinity;
-  },
-  calculateNumber(type, a, b) {
-    const numA = Number(a);
-    const numB = Number(b);
-
-    if (Number.isNaN(numA) || Number.isNaN(numB))
-      throw new TypeError('Parameters must be numbers or able to coerce to number');
-
-    switch (type) {
-      case 'SUM':
-        return Math.round(numA) + Math.round(numB);
-      case 'SUBTRACT':
-        return Math.round(numA) - Math.round(numB);
-      case 'DIVIDE':
-        if (Math.round(numB) === 0) return 'ERROR';
-        const quotient = Math.round(numA) / Math.round(numB);
-        return this.isNegZero(quotient) ? 0 : quotient;
-      default:
-        throw new Error('Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".');
+    isNegZero(n) {
+      const num = Number(n);
+      return num === 0 && 1 / num === -Infinity;
+    },
+    calculateNumber(type, a, b = 0) {
+      let aNum = Number(a);
+      let bNum = Number(b);
+  
+      if (Number.isNaN(aNum) || Number.isNaN(bNum))
+        throw TypeError('Parameters must be numbers or able to coerce to number');
+  
+      aNum = Math.round(aNum);
+      bNum = Math.round(bNum);
+  
+      let quotient;
+  
+      switch (type) {
+        case 'SUM':
+          return aNum + bNum;
+        case 'SUBTRACT':
+          return aNum - bNum;
+        case 'DIVIDE':
+          if (bNum === 0) return 'ERROR';
+          quotient = aNum / bNum;
+          return this.isNegZero(quotient) ? 0 : quotient;
+        default:
+          throw Error(
+            'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+          );
+      }
     }
-  }
-};
-
-module.exports = Utils;
+  };
+  
+  module.exports = Utils;
