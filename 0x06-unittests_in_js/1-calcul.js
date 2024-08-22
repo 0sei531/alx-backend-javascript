396f8f9c6032822ead6e203f392615417adfc49b
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-22 10:43:31 -1200

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -11,27 +11,24 @@ module.exports = function calculateNumber(type, a, b = 0) {
   let aNum = Number(a);
   let bNum = Number(b);
 
-  if (Number.isNaN(aNum) || Number.isNaN(bNum)) {
-    throw new TypeError('Parameters must be numbers or coercible to numbers');
-  }
+  if (Number.isNaN(aNum) || Number.isNaN(bNum))
+    throw TypeError('Parameters must be numbers or able to coerce to number');
 
-  // Round the numbers before performing the operation
   aNum = Math.round(aNum);
   bNum = Math.round(bNum);
 
   switch (type) {
     case SUM:
       return aNum + bNum;
-
     case SUBTRACT:
       return aNum - bNum;
-
     case DIVIDE:
-      if (bNum === 0) return 'ERROR'; // Handle division by zero
+      if (bNum === 0) return 'ERROR';
       const quotient = aNum / bNum;
-      return isNegZero(quotient) ? 0 : quotient; // Handle negative zero case
-
+      return isNegZero(quotient) ? 0 : quotient;
     default:
-      throw new Error('Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".');
+      throw Error(
+        'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+      );
   }
 };
